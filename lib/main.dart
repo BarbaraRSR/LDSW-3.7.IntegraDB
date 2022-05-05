914adb5cb14158eeac684e1fa8a29b55f69030de
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-05-04 20:06:29 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -24,7 +28,21 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FutureBuilder(
+        future: _fbApp,
+        builder: (context, snapshot) {
+          íf (snapshot.hasError) {
+            print ('Hay un error :( ${snapshot.error.toString()}');
+            return Text('Hubo un error :(');
+          } else if (snapshot.hasData) {
+            return MyHomePage(title: 'Flutter Demo Home Page');
+          } else {
+            return Center (
+              child: CircularProgressIndicator (),
+            );
+          }
+        },
+      )
     );
   }
 }
@@ -51,6 +69,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    DatabaseReference _testRef = FirebaseDatabase.instance.reference().child("test");
+    _testRef.set("Hello world ${Random().nextInt(100)}");
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
